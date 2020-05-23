8b7caa82a79711a030744a11edd824b612955b7a
David Drugeon-Hamon <zebeurton@gmail.com>
2020-05-23 09:50:08 +0200

    Add public user ssh key to known_keys

--- roles/pi-secure-user/molecule/default/tests/test_add_alternate_user.py
+++ roles/pi-secure-user/molecule/default/tests/test_add_alternate_user.py
@@ -1,28 +1,27 @@
 import os
 
 import testinfra.utils.ansible_runner
+import pytest
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
 
-
-def test_alternate_user_exists(host):
-    user = host.user("apple")
+@pytest.mark.parametrize("username", [
+    ("apple"),
+    ("orange")
+])
+def test_alternate_user_exists(host, username):
+    user = host.user(username)
     groups = ["adm","dialout","cdrom","sudo","audio","video","plugdev","games","users","input","netdev","gpio","i2c","spi", "pi"]
 
     assert user.exists
-    assert user.home == "/home/apple"
-    assert user.shell == "/bin/sh"
     assert all(current in user.groups for current in groups)
-    assert user.uid == 1001
+    assert user.home == "/home/"+username
 
-    user = host.user("orange")
-    groups = ["adm", "dialout", "cdrom", "sudo", "audio", "video", "plugdev",
-              "games", "users", "input", "netdev", "gpio", "i2c", "spi", "pi"]
-
-    assert user.exists
-    assert user.home == "/home/orange"
-    assert user.shell == "/bin/bash"
-    assert all(current in user.groups for current in groups)
-    assert user.uid == 1002
+    if username == 'apple':
+        assert user.uid == 1001
+        assert user.shell == "/bin/sh"
+    elif username == 'orange':
+        assert user.uid == 1002
+        assert user.shell == "/bin/bash"
